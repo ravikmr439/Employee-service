f83cc5464411546e8dbed6547d30b9d2957704cc
ravik <ravikmr439@gmail.com>
2026-03-21 12:18:52 +0530

    Added Custom Exception

---
+++ src/main/java/com/ravi/employee/Exception/InvalidAgeException.java
@@ -0,0 +1,9 @@
+package com.ravi.employee.Exception;
+
+public class InvalidAgeException extends RuntimeException{
+	
+	public InvalidAgeException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/ravi/employee/Exception/ResourceNotFoundException.java
@@ -0,0 +1,10 @@
+package com.ravi.employee.Exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(String message)
+	{
+		super (message);
+	}
+
+}
